1c9f3199656304da6857d86ebf6982ba39b8f9ae
Gondo0 <chlehddh8062@gmail.com>
2021-12-17 14:22:29 +0900

    백준 문제풀이

---
+++ JavaStudy/src/JavaStudyRoom/BackJoon_001.java
@@ -0,0 +1,15 @@
+package JavaStudyRoom;
+/*
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+ */
+public class BackJoon_001 {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
